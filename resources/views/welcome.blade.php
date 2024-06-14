f0d9d7930c67998fd870d471ec6a57974cfe28cc
Vigilio <yonatanvigiliolavado09@gmail.com>
2024-06-14 01:03:03 -0500

    new changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        @vite(["resources/ts/main.ts","resources/css/main.css"])
     </head>
     <body class="antialiased">
        <h1>Hola mundo 3</h1>
